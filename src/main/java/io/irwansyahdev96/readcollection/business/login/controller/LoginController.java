6772720b7e7c4cc7519056d3eb5f69a9aff79e52
irwansyah10010 <irwanbae33@gmail.com>
2025-08-21 16:49:58 +0700

    base feature login

--- src/main/java/io/irwansyahdev96/readcollection/business/login/controller/LoginController.java
+++ src/main/java/io/irwansyahdev96/readcollection/business/login/controller/LoginController.java
@@ -17,9 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.irwansyahdev96.readcollection.business.login.LoginDto;
+import io.irwansyahdev96.readcollection.business.user.model.User;
 import io.irwansyahdev96.readcollection.business.user.service.UserService;
-import io.irwansyahdev96.readcollection.dto.LoginDto;
-import io.irwansyahdev96.readcollection.model.User;
+
 import io.irwansyahdev96.readcollection.util.JwtUtil;
 
 @RestController
@@ -49,12 +50,12 @@ public class LoginController {
         cal.add(Calendar.HOUR_OF_DAY, 1);
 
         claims.put("exp", cal.getTime());
-        claims.put("id", user.getId());
+        claims.put("id", user.getUsername());
         claims.put("rc", user.getRole());
 
         Map<String, String> message = new HashMap<>();
 
-        message.put("id", user.getId());
+        message.put("id", user.getUsername());
         message.put("name", user.getName());
         message.put("role",user.getRole());
         message.put("token",jwtUtil.generated(claims));
